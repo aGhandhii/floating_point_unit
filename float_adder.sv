be1f68ba1370649bde3a29d009b35cb90aaae707
Alex Ghandhi <alexpghandhi@gmail.com>
2024-01-26 16:15:05 -0800

    begin adder hardware implementation
    this version is fixed for the single-precision float format

--- float_adder.sv
+++ float_adder.sv
@@ -63,6 +63,81 @@ module float_adder #(
     assign mantissa_a = a[MANTISSA_SIZE-1:0];
     assign mantissa_b = b[MANTISSA_SIZE-1:0];
 
+    // Internal Control Signals
+    logic EB_greater, E_equal, MA_greater, MX_greater, sameSign, m_add_adjust;
+    logic [1:0] inexact_portions;
+    logic [EXPONENT_SIZE:0] E_shamt;
+    logic [MANTISSA_SIZE+1:0] mantissa_x, mantissa_y;
+
+    // Simple Control Signal Definitions
+    assign inexact = |inexact_portions;  // OR the bits together
+    assign MA_greater = (mantissa_a > mantissa_b) ? 1'b1 : 1'b0;
+    assign MX_greater = (mantissa_x > mantissa_y) ? 1'b1 : 1'b0;
+    assign sameSign = ~(sign_a ^ sign_b);
+
+    // Sign Unit
+    always_comb begin
+        if ((~EB_greater) & (~E_equal)) begin
+            sign_out = sign_a;
+        end else begin
+            if (EB_greater) begin
+                sign_out = sign_b;
+            end else begin
+                if (MA_greater) begin
+                    sign_out = sign_a;
+                end else begin
+                    sign_out = sign_b;
+                end
+            end
+        end
+    end  // Sign Unit
+
+    // EXPONENT CALCULATIONS
+
+    // Intermediate Logic
+    logic [EXPONENT_SIZE:0] E_base, E_diff_raw, E_m_add;
+
+    // Signal Assignments
+    assign E_diff_raw = {1'b0, exponent_a} - {1'b0, exponent_b};
+    assign EB_greater = E_diff_raw[EXPONENT_SIZE];
+    assign E_equal = (E_diff_raw == 0) ? 1'b1 : 1'b0;
+    assign E_shamt = EB_greater ? (~E_diff_raw + 1) : E_diff_raw;
+    assign E_base = EB_greater ? {1'b0, exponent_b} : {1'b0, exponent_a};
+    assign E_m_add = m_add_adjust ? (E_base + 1) : E_base;
+    assign overflow = E_m_add[EXPONENT_SIZE];
+
+    // Final adjustment (handle leading zeros for mantissa subtraction)
+    // Currently set for Single-Precision values
+    assign {underflow, exponent_out} = E_m_add - {4'b0000, leadingZeros};
+
+    ///////////////////////////
+    // MANTISSA CALCULATIONS //
+    ///////////////////////////
+
+    // EXPONENT ADJUSTMENT STAGE
+    // In this stage, we shift the mantissas so that they represent the same
+    // exponent, allowing for a simple arithmetic operation in the next stage.
+    // Our input a and b will be converted to x and y, representing the order
+    // with weight to the value of the input exponent.
+
+    // Intermediate Logic
+    logic [MANTISSA_SIZE+1:0] exponentShiftIn;
+
+    // Logic assignments
+    assign mantissa_x = EB_greater ? {2'b01, mantissa_b} : {2'b01, mantissa_a};
+
+    // Adjust the lower mantissa by the difference in exponents
+    assign mantissa_y = exponentShiftIn >> E_shamt;
+    always_comb begin
+        if (E_shamt > MANTISSA_SIZE) begin
+            inexact_portions[0] = |exponentShiftIn[MANTISSA_SIZE-1:0];
+        end else begin
+            if (E_shamt == 0) inexact_portions[0] = 0;
+            else inexact_portions[0] = |exponentShiftIn[E_shamt-1:0];
+        end
+    end
+
+    // MANTISSA ADDITION/SUBTRACTION STAGE
 
 
 endmodule  // float_adder
